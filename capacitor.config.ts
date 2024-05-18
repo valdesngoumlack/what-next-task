48a6a40942abeede9cbadcafe87b1ffab8b9bb93
Valdes NGOUMLACK <valdes.ngoumlack@gmail.com>
2024-05-18 17:10:14 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'WhatNext Task',
+  webDir: 'www'
+};
+
+export default config;
